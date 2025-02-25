15ef84d23e2568b8d2c9dfd23213f96296b1eac4
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-02-25 16:34:17 +0300

    create newGridHandler function

--- js.js
+++ js.js
@@ -2,16 +2,22 @@ const container = document.getElementById('container')
 const headerTitle = document.createElement('h1')
 const btnCreateNewGrid = document.createElement('button')
 const gridContainer = document.createElement('div')
-const gridItemSize = 580
+const gridContainerSize = 580
 
 let gridNumber = 20
-let gridSize = gridItemSize / gridNumber
+let gridSize = gridContainerSize / gridNumber
 
+// CLASS ATTRIBUTE
 headerTitle.setAttribute('class','title')
 gridContainer.setAttribute('class','gridContainer')
 
+// GRID CONTAINER
+gridContainer.style.width=`${gridContainerSize}px`
+gridContainer.style.height=`${gridContainerSize}px`
+
 headerTitle.innerText='Etch A Sketch'
 btnCreateNewGrid.textContent='New Grid'
+btnCreateNewGrid.addEventListener('click', ()=>newGridHandler())
 
 container.append(
     headerTitle,
@@ -19,8 +25,23 @@ container.append(
     gridContainer
 )
 
-gridContainer.style.width=`${gridItemSize}px`
-gridContainer.style.height=`${gridItemSize}px`
+function newGridHandler () {
+    let newGrid = parseInt(prompt('Enter a new value between 1-100 :'))
+    if(isNaN(newGrid)){
+        alert('Invalid number.\nPlease try again!')
+        newGridHandler()
+    }
+    else{
+        if(newGrid > 100){
+            alert('The value must be less than 100.\nPlease try again!')
+            newGridHandler()
+        }
+        else{
+            alert(`change grid content ${newGrid}`)
+        }
+    }
+}
+
 for (let index = 0; index < (gridNumber * gridNumber); index++) {
     const gridBox = document.createElement('div')
     gridBox.style.width = `${gridSize}px`
